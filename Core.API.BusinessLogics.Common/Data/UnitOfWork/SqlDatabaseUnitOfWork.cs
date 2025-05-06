b27a2e06a36b634e344d47fa4ed2c536afff9776
RajeshNandhan <rajeshn7k@gmail.com>
2025-05-06 12:11:14 +0530

    version update from 8 to 7 for github dotnet.yml file

--- Core.API.BusinessLogics.Common/Data/UnitOfWork/SqlDatabaseUnitOfWork.cs
+++ Core.API.BusinessLogics.Common/Data/UnitOfWork/SqlDatabaseUnitOfWork.cs
@@ -8,22 +8,24 @@ namespace Core.API.BusinessLogics.Common
 {
     public class SqlDatabaseUnitOfWork : IUnitOfWork
     {
-        private readonly DbContext sqlDbContext;
+        private readonly DbContext SqlDbContext;
 
         public IGenericRepository<Book> BookRepository { get; }
 
         public IGenericRepository<Person> PersonRepository { get; }
 
-        public SqlDatabaseUnitOfWork(SqlDataBaseDataContext sqlDbContext, IGenericRepository<Book> bookRepository, IGenericRepository<Person> personRepository)
+        public SqlDatabaseUnitOfWork(SqlDataBaseDataContext sqlDbContext,
+            IGenericRepository<Book> bookRepository,
+            IGenericRepository<Person> personRepository)
         {
-            this.sqlDbContext = sqlDbContext;
-            this.BookRepository = bookRepository;
-            this.PersonRepository = personRepository;
+            SqlDbContext = sqlDbContext;
+            BookRepository = bookRepository;
+            PersonRepository = personRepository;
         }
 
         public virtual Task<int> CommitAsync(CancellationToken cancellationToken)
         {
-            return sqlDbContext.SaveChangesAsync(cancellationToken);
+            return SqlDbContext.SaveChangesAsync(cancellationToken);
         }
     }
 }
